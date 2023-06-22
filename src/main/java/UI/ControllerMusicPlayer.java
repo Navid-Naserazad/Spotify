1fd075fd00521835df38dbea3cbdd69ef306bc8f
Navid-Naserazad <126415333+Navid-Naserazad@users.noreply.github.com>
2023-06-23 01:50:14 +0430

    added lyrics scene

--- src/main/java/UI/ControllerMusicPlayer.java
+++ src/main/java/UI/ControllerMusicPlayer.java
@@ -3,6 +3,7 @@ package UI;
 import Artist.Music;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -10,11 +11,13 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ProgressBar;
 import javafx.scene.control.Slider;
+import javafx.scene.image.Image;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 //import javafx.scene.media.Media;
 //import javafx.scene.media.MediaPlayer;
 import java.io.File;
+import java.io.IOException;
 import java.net.URL;
 import java.nio.file.attribute.AclEntryType;
 import java.util.ArrayList;
@@ -65,7 +68,26 @@ public class ControllerMusicPlayer implements Initializable {
         songName.setText(getMusic().getTitle());
     }
 
-    public void showLyrics(ActionEvent event) {
-
+    public void showLyrics(ActionEvent event) throws IOException {
+        Stage stage = new Stage();
+        stage.setTitle("Spotify");
+        stage.getIcons().add(new Image("D:\\SBU\\Term 2\\AP\\Assignments\\Spotify\\src\\main\\resources\\UI\\spotify-icon-marilyn-scott-0.png"));
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("showLyrics.fxml"));
+        loader.setControllerFactory(type -> {
+            if (type == ControllerShowLyrics.class) {
+                return new ControllerShowLyrics();
+            }
+            try {
+                return type.getConstructor().newInstance();
+            }
+            catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        });
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.showAndWait();
     }
 }
--- src/main/java/UI/ControllerShowLyrics.java
+++ src/main/java/UI/ControllerShowLyrics.java
@@ -1,4 +1,27 @@
 package UI;
 
-public class ControllerShowLyrics {
+import Shared.UserRequest;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ControllerShowLyrics implements Initializable {
+    // Attributes
+    Parent root;
+    Stage stage;
+    Scene scene;
+    @FXML
+    private Label lyrics;
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
 }
