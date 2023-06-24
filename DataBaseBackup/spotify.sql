494751e1481530769b01689afa5b40af40b52136
Bardia <Bardiaa830@gmail.com>
2023-06-24 20:18:26 +0330

    db backup

--- DataBaseBackup/spotify.sql
+++ DataBaseBackup/spotify.sql
@@ -42,6 +42,28 @@ INSERT INTO `artist` VALUES ('9cac3f6c-9638-48e6-af68-993485ec4fdb','jdjdjdj','a
 UNLOCK TABLES;
 
 --
+-- Table structure for table `liked`
+--
+
+DROP TABLE IF EXISTS `liked`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `liked` (
+  `user_id` text NOT NULL,
+  `track_id` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `liked`
+--
+
+LOCK TABLES `liked` WRITE;
+/*!40000 ALTER TABLE `liked` DISABLE KEYS */;
+/*!40000 ALTER TABLE `liked` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `music`
 --
 
@@ -183,8 +205,52 @@ CREATE TABLE `user` (
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES ('918a0df8-4a35-4128-9d2a-00a484b3dabd','ooooo','00000','hhh'),('b5b887e0-5fc3-43d7-8b54-bc43b6aa4839','kekek','jejj','kekek'),('e324e2cb-7385-41fc-9074-61f472608de8','slsll','s;;s;',';ss;'),('054e3938-9710-41bb-9198-cc0e2555adaa','jddj','jdjdj','jjdjd'),('0810aa59-f854-4b60-bef4-ee7a0a4193c4','slls','ksks','ksks'),('986bd345-9c65-490d-b806-1a5670ad23d2','lslslls','lsls',';s;;s'),('db8bd9f9-238b-4abe-a244-9d1fa8dad5d6','KDLD','DLLD','DLLD'),('6c20aa52-b8ca-440d-9e1f-eaa72dc4beab','bardia','akbari','djdjdj'),('8c0be978-7066-4b91-b73f-331311923994','kdkd','ososos','ppapapa');
+INSERT INTO `user` VALUES ('918a0df8-4a35-4128-9d2a-00a484b3dabd','ooooo','00000','hhh'),('b5b887e0-5fc3-43d7-8b54-bc43b6aa4839','kekek','jejj','kekek'),('e324e2cb-7385-41fc-9074-61f472608de8','slsll','s;;s;',';ss;'),('054e3938-9710-41bb-9198-cc0e2555adaa','jddj','jdjdj','jjdjd'),('0810aa59-f854-4b60-bef4-ee7a0a4193c4','slls','ksks','ksks'),('986bd345-9c65-490d-b806-1a5670ad23d2','lslslls','lsls',';s;;s'),('db8bd9f9-238b-4abe-a244-9d1fa8dad5d6','KDLD','DLLD','DLLD'),('6c20aa52-b8ca-440d-9e1f-eaa72dc4beab','bardia','akbari','djdjdj'),('8c0be978-7066-4b91-b73f-331311923994','kdkd','ososos','ppapapa'),('7d718b6b-0883-41a2-bb12-d60c714826ce','nariman','1234','g');
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user_artist_follow`
+--
+
+DROP TABLE IF EXISTS `user_artist_follow`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user_artist_follow` (
+  `user_id` text NOT NULL,
+  `artist_id` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user_artist_follow`
+--
+
+LOCK TABLES `user_artist_follow` WRITE;
+/*!40000 ALTER TABLE `user_artist_follow` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user_artist_follow` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user_user_follow`
+--
+
+DROP TABLE IF EXISTS `user_user_follow`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user_user_follow` (
+  `user_id_1` text,
+  `user_id_2` text
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user_user_follow`
+--
+
+LOCK TABLES `user_user_follow` WRITE;
+/*!40000 ALTER TABLE `user_user_follow` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user_user_follow` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -196,4 +262,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-06-23 22:55:09
+-- Dump completed on 2023-06-24 20:18:00
